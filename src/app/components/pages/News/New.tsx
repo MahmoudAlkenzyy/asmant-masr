1b0e8bbbfcec3f7235a7d22a928f447b1bddfe58
MahmoudAlkenzyy <Mahmoudalkenzy2@gmail.com>
2025-10-29 11:34:06 +0300

    $

--- src/app/components/pages/News/New.tsx
+++ src/app/components/pages/News/New.tsx
@@ -5,11 +5,11 @@ const New = () => {
   return (
     <div className="grid grid-cols-4  items-center gap-6 ">
       <div className="col-span-1 bg-primary">
-        <NewsTab />
-        <NewsTab />
-        <NewsTab />
-        <NewsTab />
-        <NewsTab />
+        <NewsTab id="bab5588e-d381-41a4-e65f-08de14638c7a" />
+        <NewsTab id="bab5588e-d381-41a4-e65f-08de14638c7a" />
+        <NewsTab id="bab5588e-d381-41a4-e65f-08de14638c7a" />
+        <NewsTab id="bab5588e-d381-41a4-e65f-08de14638c7a" />
+        <NewsTab id="bab5588e-d381-41a4-e65f-08de14638c7a" />
       </div>
       <div className="col-span-3"></div>
     </div>
